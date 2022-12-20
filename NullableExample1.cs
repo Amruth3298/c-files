b39b58c285bb831f30bac51fd75abc77b2d30a83
Amruth3298 <120582423+Amruth3298@users.noreply.github.com>
2022-12-20 16:08:49 -0600

    Add files via upload

---
+++ NullableExample1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace projects
+{
+    class NullableExample1
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> a = 10;
+            Nullable<double> d = 10.10;
+            Nullable<char> c = 'S';
+            Nullable<bool> b = false;
+            
+            Console.WriteLine(a.Value);
+
+            a = null;
+            d = null;
+            c = null;
+            b = null; 
+            if (a.HasValue)
+            {
+                Console.WriteLine(a.Value);
+            }
+            if (a == null)
+                Console.WriteLine("It contains null value");
+        }
+    }
+}
---
+++ NullableExample2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace projects
+{
+    class NullableExample2
+    {
+        static void Main(string[] args)
+        {
+             
+            int? a = 10;
+             
+            double? f = 10.10;
+           
+            bool? b = false;
+            
+            char? c = 'S';
+           
+            if (a.HasValue)
+            {
+                Console.WriteLine(a.Value);
+            }
+            else Console.WriteLine("a contains null value");
+
+            a = null;
+            if (a.HasValue) 
+            {
+                Console.WriteLine(a.Value);
+            }
+            else Console.WriteLine("a contains null value");
+        }
+    }
+
+}
+
