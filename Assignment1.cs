02f808a4f490540e38adf4b5f7792c57d27d7e9f
Amruth3298 <120582423+Amruth3298@users.noreply.github.com>
2023-01-03 19:31:11 -0600

    Add files via upload

---
+++ Assignment1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Projects
+{
+    class Assignment1
+    {
+        static int Divisible(int num1, int num2)
+        {
+            return (num1 % 2 == 0 && num2 % 2 == 0 || num1 % 3 == 0 && num2 % 3 == 0) ?
+                num1 * num2 : num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divisible(12, 6));
+            Console.WriteLine(Divisible(5, 7));
+        }
+    }
+}
+
---
+++ Assignment2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Projects
+{
+    class Program2
+    {
+        static int[] a = { 17, 3, 76, 98, 24 };
+        static int maxnum()
+        {
+            int i;
+            int max = a[0];
+            for (i = 1; i<a.Length; i++)
+                if (a[i] > max)
+                max = a[i];
+            return max;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(" Largest number from the given array is " +maxnum());
+        }
+    }
+}
